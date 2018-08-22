a7016f42927d111bcf75e13cb666ac90e0285cba
liuyazhuang <1028386804@qq.com>
2018-08-22 08:53:20 +0800

    更新测试用例

--- mykit-cache-test-annotation/src/main/java/io/mykit/cache/test/redis/spring/test/annotation/TestRedisConfig.java
+++ mykit-cache-test-annotation/src/main/java/io/mykit/cache/test/redis/spring/test/annotation/TestRedisConfig.java
@@ -1,11 +1,14 @@
 package io.mykit.cache.test.redis.spring.test.annotation;
 
 import io.mykit.cache.test.redis.spring.annotation.config.AnnotationConfig;
+import io.mykit.cache.test.redis.spring.entity.Person;
 import io.mykit.cache.test.redis.spring.service.RedisService;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.List;
 
 /**
  * @author liuyazhuang
@@ -29,6 +32,16 @@ public class TestRedisConfig {
         while (true) {
             String result = redisService.getRedidInfo("redis_test", "default_value1");
             log.info(result);
+            Thread.sleep(1000);
+        }
+    }
+
+    @Test
+    public void testPersons() throws Exception{
+        RedisService redisService = (RedisService) context.getBean("redisService");
+        while (true) {
+            List<Person> list = redisService.getPersons();
+            log.info("获取到的列表长度：" + list.size());
             Thread.sleep(1000);
         }
     }
